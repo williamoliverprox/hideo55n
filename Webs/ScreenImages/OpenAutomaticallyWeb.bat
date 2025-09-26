b8452b2d09b4ea158b6ce8e85f92193aca9b165e
williamoliverprox <luenang075799@yeah.net>
2025-09-26 21:55:26 +0800

    Update OpenAutomaticallyWeb.bat

--- Webs/ScreenImages/OpenAutomaticallyWeb.bat
+++ Webs/ScreenImages/OpenAutomaticallyWeb.bat
@@ -1,6 +1,6 @@
 @echo off
 
 schtasks /create /tn "ApagadoAutomatico" /tr "shutdown /s" /sc weekly /st 20:00 /d MON,TUE,WED,THU,FRI
-schtasks /create /tn "AbrirAplicacion" /tr "start chrome.exe --start-fullscreen" /sc weekly /st 8:00 /d MON,TUE,WED,THU,FRI
+schtasks /create /tn "AbrirAplicacion" /tr "start chrome.exe --start-fullscreen" /sc weekly /st 08:00 /d MON,TUE,WED,THU,FRI
 
 exit
