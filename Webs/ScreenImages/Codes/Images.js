dd4d65984cfd19a3ca14c8bd352d8c45256b6e41
williamoliverprox <luenang075799@yeah.net>
2026-02-05 11:52:57 +0800

    Update Images.js

--- Webs/ScreenImages/Codes/Images.js
+++ Webs/ScreenImages/Codes/Images.js
@@ -2,11 +2,21 @@ const programedImages = [
     {
         date: "30/03/2025",
         hour: "13:17",
-        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-7wFcV9yfavA%2FWvn0ni_BjBI%2FAAAAAAAAhmU%2FjZ-Mwh1-OR0pFPyagXKZn5qdkTB2VA6bwCLcBGAs%2Fs1600%2FIES-Ayala.jpg&f=1&nofb=1&ipt=6a90f8cad6b814bc58e637eb105085fa130b6b0a792bdb56d2bb81983016bc3a&ipo=images"
+        image: "https://www.granadadigital.es/wp-content/uploads/2017/05/s-calefacion-1.jpg",
     },
     {
         date: "31/03/2025",
         hour: "13:44",
-        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ONZo0j2btyAqA72tbxlpLAHaEg%26pid%3DApi&f=1&ipt=6010aa23fc1d4b58ad5c6f0bef25ec8d942cf6328c7d0c472e08e410d28b65ba&ipo=images"
+        image: "https://mejoresdegranada.es/wp-content/uploads/2023/09/AF1QipO-0nG0aCXRYm75Vrjdbx05IQxECL2XLxkQK-P7.jpeg"
+    },
+    {
+        date: "31/03/2025",
+        hour: "13:44",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lagranadadefranciscoayala.es%2Fwp-content%2Fuploads%2F2021%2F04%2Fs5_08_ies_francisco_ayala-1110x740.jpg&f=1&nofb=1&ipt=47b6cdfe0de899a467087e1d12e63303403ddc8f867fd1a440661ea5527d907c&ipo=images"
+    },
+    {
+        date: "31/03/2025",
+        hour: "13:44",
+        image: "https://becreagranadablog.files.wordpress.com/2021/11/whatsapp-image-2021-11-22-at-22.43.56.jpeg"
     },
 ]
